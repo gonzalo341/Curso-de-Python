12787ae974f9ad159eac9f3b2cd78b12343837c1
Gonzalo Centeno <gonzalo.centeno.84@gmail.com>
2024-12-11 20:33:54 -0300

    commit

--- snake.py
+++ snake.py
@@ -5,6 +5,16 @@ import turtle
 import time
 import random
 
+segmentos = [] # Segmentos de la serpiente guardados
+
+# Valor inicial del Puntaje, Puntaje Maximo, Nivel y Velocidad.
+puntaje = 0
+puntaje_maximo = 0
+nivel = 1
+velocidad = 0.1
+
+obstaculos = []
+
 pantalla = turtle.Screen()
 pantalla.title("Snake")
 pantalla.bgcolor("orange")
@@ -18,7 +28,7 @@ if not nombre_jugador:
 
 #.strip() = borra los espacios del inicio y del final del prompt ingresado
 #.lower() = pasa todas las mayusculas a minusculas del prompt ingresado
-#\n = salto de linea
+#/n = salto de linea
 #''.join() = signo que separa las lista
 
 # Elegir el color de la serpiente
@@ -51,30 +61,116 @@ for _ in range (4):
 linea_limite.hideturtle()
 
 # Registrar shape personalizado para la cabeza de la serpiente
-turtle.addshape("Diseños/Snake-Head-Up.gif")
-turtle.addshape("Diseños/Snake-Head-Down.gif")
-turtle.addshape("Diseños/Snake-Head-Right.gif")
-turtle.addshape("Diseños/Snake-Head-Left.gif")
-turtle.addshape("Diseños/Snake-Body-Vertical.gif")
-turtle.addshape("Diseños/Snake-Body-Horizontal.gif")
+# Color Verde
+turtle.addshape("Diseños/Darkgreen/Snake-Body-Horizontal.gif")
+turtle.addshape("Diseños/Darkgreen/Snake-Body-Vertical.gif")
+turtle.addshape("Diseños/Darkgreen/Snake-Head-Down.gif")
+turtle.addshape("Diseños/Darkgreen/Snake-Head-Left.gif")
+turtle.addshape("Diseños/Darkgreen/Snake-Head-Right.gif")
+turtle.addshape("Diseños/Darkgreen/Snake-Head-Up.gif")
+# Color Azul
+turtle.addshape("Diseños/Azul/Snake-Body-Horizontal-Azul.gif")
+turtle.addshape("Diseños/Azul/Snake-Body-Vertical-Azul.gif")
+turtle.addshape("Diseños/Azul/Snake-Head-Down-Azul.gif")
+turtle.addshape("Diseños/Azul/Snake-Head-Left-Azul.gif")
+turtle.addshape("Diseños/Azul/Snake-Head-Right-Azul.gif")
+turtle.addshape("Diseños/Azul/Snake-Head-Up-Azul.gif")
+# Color Rojo
+turtle.addshape("Diseños/Rojo/Snake-Body-Horizontal-Rojo.gif")
+turtle.addshape("Diseños/Rojo/Snake-Body-Vertical-Rojo.gif")
+turtle.addshape("Diseños/Rojo/Snake-Head-Down-Rojo.gif")
+turtle.addshape("Diseños/Rojo/Snake-Head-Left-Rojo.gif")
+turtle.addshape("Diseños/Rojo/Snake-Head-Right-Rojo.gif")
+turtle.addshape("Diseños/Rojo/Snake-Head-Up-Rojo.gif")
+#Color Purpura
+turtle.addshape("Diseños/Purpura/Snake-Body-Horizontal-Purpura.gif")
+turtle.addshape("Diseños/Purpura/Snake-Body-Vertical-Purpura.gif")
+turtle.addshape("Diseños/Purpura/Snake-Head-Down-Purpura.gif")
+turtle.addshape("Diseños/Purpura/Snake-Head-Left-Purpura.gif")
+turtle.addshape("Diseños/Purpura/Snake-Head-Right-Purpura.gif")
+turtle.addshape("Diseños/Purpura/Snake-Head-Up-Purpura.gif")
+# Color Naranja
+turtle.addshape("Diseños/Naranja/Snake-Body-Horizontal-Naranja.gif")
+turtle.addshape("Diseños/Naranja/Snake-Body-Vertical-Naranja.gif")
+turtle.addshape("Diseños/Naranja/Snake-Head-Down-Naranja.gif")
+turtle.addshape("Diseños/Naranja/Snake-Head-Left-Naranja.gif")
+turtle.addshape("Diseños/Naranja/Snake-Head-Right-Naranja.gif")
+turtle.addshape("Diseños/Naranja/Snake-Head-Up-Naranja.gif")
+# Color Amarillo
+turtle.addshape("Diseños/Amarillo/Snake-Body-Horizontal-Amarillo.gif")
+turtle.addshape("Diseños/Amarillo/Snake-Body-Vertical-Amarillo.gif")
+turtle.addshape("Diseños/Amarillo/Snake-Head-Down-Amarillo.gif")
+turtle.addshape("Diseños/Amarillo/Snake-Head-Left-Amarillo.gif")
+turtle.addshape("Diseños/Amarillo/Snake-Head-Right-Amarillo.gif")
+turtle.addshape("Diseños/Amarillo/Snake-Head-Up-Amarillo.gif")
 # Usar archivo .gif
+
+color_head_up = "Diseños/Darkgreen/Snake-Head-Up.gif"
+
+match color_serpiente:
+    case "darkgreen":
+        color_head_horizontal = "Diseños/Darkgreen/Snake-Body-Horizontal.gif"
+        color_head_vertical = "Diseños/Darkgreen/Snake-Body-Vertical.gif"
+        color_head_down = "Diseños/Darkgreen/Snake-Head-Down.gif"
+        color_head_left = "Diseños/Darkgreen/Snake-Head-Left.gif"
+        color_head_right = "Diseños/Darkgreen/Snake-Head-Right.gif"
+        color_head_up = "Diseños/Darkgreen/Snake-Head-Up.gif"
+    case "blue": 
+        color_head_horizontal = "Diseños/Azul/Snake-Body-Horizontal-Azul.gif"
+        color_head_vertical = "Diseños/Azul/Snake-Body-Vertical-Azul.gif"
+        color_head_down = "Diseños/Azul/Snake-Head-Down-Azul.gif"
+        color_head_left = "Diseños/Azul/Snake-Head-Left-Azul.gif"
+        color_head_right = "Diseños/Azul/Snake-Head-Right-Azul.gif"
+        color_head_up = "Diseños/Azul/Snake-Head-Up-Azul.gif"
+    case "red":
+        color_head_horizontal = "Diseños/Rojo/Snake-Body-Horizontal-Rojo.gif"
+        color_head_vertical = "Diseños/Rojo/Snake-Body-Vertical-Rojo.gif"
+        color_head_down = "Diseños/Rojo/Snake-Head-Down-Rojo.gif"
+        color_head_left = "Diseños/Rojo/Snake-Head-Left-Rojo.gif"
+        color_head_right = "Diseños/Rojo/Snake-Head-Right-Rojo.gif"
+        color_head_up = "Diseños/Rojo/Snake-Head-Up-Rojo.gif"
+    case "purple":
+        color_head_horizontal = "Diseños/Purpura/Snake-Body-Horizontal-Purpura.gif"
+        color_head_vertical = "Diseños/Purpura/Snake-Body-Vertical-Purpura.gif"
+        color_head_down = "Diseños/Purpura/Snake-Head-Down-Purpura.gif"
+        color_head_left = "Diseños/Purpura/Snake-Head-Left-Purpura.gif"
+        color_head_right = "Diseños/Purpura/Snake-Head-Right-Purpura.gif"
+        color_head_up = "Diseños/Purpura/Snake-Head-Up-Purpura.gif"
+    case "orange":
+        color_head_horizontal = "Diseños/Naranja/Snake-Body-Horizontal-Naranja.gif"
+        color_head_vertical = "Diseños/Naranja/Snake-Body-Vertical-Naranja.gif"
+        color_head_down = "Diseños/Naranja/Snake-Head-Down-Naranja.gif"
+        color_head_left = "Diseños/Naranja/Snake-Head-Left-Naranja.gif"
+        color_head_right = "Diseños/Naranja/Snake-Head-Right-Naranja.gif"
+        color_head_up = "Diseños/Naranja/Snake-Head-Up-Naranja.gif"
+    case "yellow":
+        color_head_horizontal = "Diseños/Amarillo/Snake-Body-Horizontal-Amarillo.gif"
+        color_head_vertical = "Diseños/Amarillo/Snake-Body-Vertical-Amarillo.gif"
+        color_head_down = "Diseños/Amarillo/Snake-Head-Down-Amarillo.gif"
+        color_head_left = "Diseños/Amarillo/Snake-Head-Left-Amarillo.gif"
+        color_head_right = "Diseños/Amarillo/Snake-Head-Right-Amarillo.gif"
+        color_head_up = "Diseños/Amarillo/Snake-Head-Up-Amarillo.gif"
 
 # Cabeza de la serpiente
 serpiente = turtle.Turtle()
 serpiente.speed(0)
-serpiente.shape("Diseños/Snake-Head-Up.gif")  # Shape personalizado
-serpiente.color(color_serpiente)
+serpiente.shape(color_head_up)  # Shape personalizado
 serpiente.penup()
 serpiente.goto(0, 0)
 serpiente.direction = "stop"
 
-segmentos = [] # Segmentos de la serpiente guardados
+# Obstaculos
 
-# Valor inicial del Puntaje, Puntaje Maximo, Nivel y Velocidad.
-puntaje = 0
-puntaje_maximo = 0
-nivel = 1
-velocidad = 0.1
+for i in range (3):
+    obstaculo = turtle.Turtle()
+    obstaculo.speed(0)
+    obstaculo.shape("circle")
+    obstaculo.color("red")
+    obstaculo.penup()
+    obstaculo.pensize(10)
+    obstaculo.direction = random.choice(["up","down","left","right"])
+    obstaculo.goto(random.randint(-230,230),random.randint(-230,230))
+    obstaculos.append(obstaculo)
 
 #comida para la serpiente
 comida = turtle.Turtle()
@@ -103,10 +199,46 @@ def actualizar_shapes_segmentos():
     for segmento in segmentos:
         # Si esta en direccion Arriba o Abajo usara el shape Vertical
         if serpiente.direction in ["up", "down"]:
-            segmento.shape("Diseños/Snake-Body-Vertical.gif")
+            segmento.shape(color_head_vertical)
         # Si esta en la direccion Derecha o Izquierda usara el shape Horizontal
         elif serpiente.direction in ["left", "right"]:
-            segmento.shape("Diseños/Snake-Body-Horizontal.gif")
+            segmento.shape(color_head_horizontal)
+
+def mover_obstaculo():
+    for obstaculo in obstaculos:
+        if obstaculo.direction == "up":
+            y = obstaculo.ycor() # Arriba
+            obstaculo.sety(y + 10)
+        elif obstaculo.direction == "down":
+                y = obstaculo.ycor() # Abajo
+                obstaculo.sety(y - 10)
+        elif obstaculo.direction == "rigth":
+                y = obstaculo.xcor() # Derecha
+                obstaculo.setx(y + 10)   
+        elif obstaculo.direction == "left":
+                y = obstaculo.xcor() # Izquierda
+                obstaculo.setx(y - 10)
+
+        if obstaculo.xcor() > 240:
+            obstaculo.setx(-240)
+        elif obstaculo.xcor() < -240:
+            obstaculo.setx(240)
+        if obstaculo.ycor() > 240:
+            obstaculo.sety(-240)
+        elif obstaculo.ycor() < -240:
+            obstaculo.sety(240)
+
+def rotation_up():
+    serpiente.shape(color_head_up)
+
+def rotation_down():
+    serpiente.shape(color_head_down)
+
+def rotation_right():
+    serpiente.shape(color_head_right)
+    
+def rotation_left():
+    serpiente.shape(color_head_left)
 
 #definir el movimiento de la serpiente
 def mover():
@@ -114,22 +246,22 @@ def mover():
     if serpiente.direction == "up":
         y = serpiente.ycor()
         serpiente.sety(y + 20)
-        serpiente.shape("Diseños/Snake-Head-Up.gif")
+        rotation_up()
     # Mover 20 px hacia Abajo
     if serpiente.direction == "down":
         y = serpiente.ycor()
         serpiente.sety(y - 20)
-        serpiente.shape("Diseños/Snake-Head-Down.gif")
+        rotation_down()
     # Mover 20 px hacia la Izquierda
     if serpiente.direction == "left":
         x = serpiente.xcor()
         serpiente.setx(x - 20)
-        serpiente.shape("Diseños/Snake-Head-Left.gif")
+        rotation_left()
     # Mover 20 px hacia Derecha
     if serpiente.direction == "right":
         x = serpiente.xcor()
         serpiente.setx(x + 20)
-        serpiente.shape("Diseños/Snake-Head-Right.gif")
+        rotation_right()
     
     actualizar_shapes_segmentos() # Al moverse se actualizara el shape de los segmentos
 
@@ -143,39 +275,47 @@ def mover():
     elif serpiente.ycor() < -240:
         serpiente.sety(240) 
 
+# Texto de "Game Over"
+texto_game_over = turtle.Turtle()
+texto_game_over.speed(0)
+texto_game_over.color("white")
+texto_game_over.penup()
+texto_game_over.hideturtle()
+
 # Mostrar el mensaje de "Game Over"
 def game_over():
+    # Esconder objetos del juego (serpiente y comida)
     serpiente.hideturtle()
-    comida.hideturtle()
     for segmento in segmentos:
         segmento.hideturtle()
+    comida.hideturtle()
+    obstaculo.hideturtle()
 
-    # Texto de "Game Over"
-    gameOver = turtle.Turtle()
-    gameOver.speed(0)
-    gameOver.color("white")
-    gameOver.penup()
-    gameOver.hideturtle()
-    gameOver.goto(0, 0)
-    gameOver.write("GAME OVER\nPresiona 'r' para reiniciar", align="center", font=("Arial", 24, "bold"))
+    texto_game_over.goto(0, 0)
+    texto_game_over.write("GAME OVER/nPresiona 'r' para reiniciar", align="center", font=("Arial", 24, "bold"))
 
 # Reiniciar el juego
 def reiniciar_juego():
     global puntaje, puntaje_maximo, nivel, velocidad
     serpiente.showturtle()
     comida.showturtle()
-    serpiente.goto(0, 0)
+    obstaculo.showturtle()
     serpiente.direction = "stop"
+    serpiente.goto(0, 0)
 
     # Ocultar los segmentos
     for segmento in segmentos:
         segmento.goto(1000,1000)
     
     segmentos.clear()  # Limpiar la lista de segmentos agregados
-    texto.clear()
+    obstaculos.clear() # Limpiar los obstaculos
+    texto_game_over.clear() # Borrar texto
 
     # Enviar la comida a una ubicación diferente
     comida.goto(random.randint(-230, 230), random.randint(-230, 230))
+
+    # Enviar el obstaculo a otra ubicacion diferente
+    obstaculo.goto(random.randint(-230,230),random.randint(-230,230))
 
     # Reiniciar puntaje, nivel y velocidad
     if puntaje > puntaje_maximo:
@@ -216,10 +356,12 @@ while True:
 
     mover()
 
+    mover_obstaculo()
+
     if serpiente.distance(comida) < 20:
         nuevo_segmento = turtle.Turtle()
         nuevo_segmento.speed(0)
-        nuevo_segmento.shape("Diseños/Snake-Body-Horizontal.gif")  # Forma inicial
+        nuevo_segmento.shape(color_head_horizontal)
         nuevo_segmento.penup()
     
         if len(segmentos) > 0:
@@ -267,5 +409,9 @@ while True:
         if segmento.distance(serpiente) < 20:
             game_over()
             break
+
+    # Si la distancia es menor de 20px pierde
+    if obstaculo.distance(serpiente) < 30:
+        game_over()
     
     time.sleep(velocidad)
